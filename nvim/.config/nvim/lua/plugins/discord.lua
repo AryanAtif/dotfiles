6c70fe15d8cf95ce1e120c6b606b611dfe8cb819
Aryan Atif <aryanatif01@gmail.com>
2026-01-16 18:40:31 +0500

    Change nvim to gruvbox

---
+++ nvim/.config/nvim/lua/plugins/discord.lua
@@ -0,0 +1,10 @@
+return {
+  -- Add a plugin by its GitHub shortname
+  { "vyfor/cord.nvim" },
+  {
+    "vyfor/cord.nvim",
+    build = ":Cord update",
+    -- opts = {}
+  },
+  -- Add a plugin with additional configuration
+}
