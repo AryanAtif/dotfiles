7645c1009730372b49fc298ee64f57fa0e5d9cfc
Aryan Atif <aryanatif01@gmail.com>
2026-05-24 07:32:46 +0500

    Add controls for the column keys and remove the memory status on the bar

--- dwm/.config/dwm/dwm-6.8/config.h
+++ dwm/.config/dwm/dwm-6.8/config.h
@@ -1,3 +1,5 @@
+#include <X11/XF86keysym.h> // for the volume keys
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -60,12 +62,18 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
+/* The volume key commands */
+static const char *up_vol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",   NULL };
+static const char *down_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",   NULL };
+static const char *mute_vol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
+
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      togglebar,      {0} }, // toggles the status bar ON/OFF
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -96,6 +104,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* The Volume Keys */
+  { 0,             XF86XK_AudioMute,         spawn,          {.v = mute_vol } },
+  { 0,             XF86XK_AudioLowerVolume,  spawn,          {.v = down_vol } },
+  { 0,             XF86XK_AudioRaiseVolume,  spawn,          {.v = up_vol } },
 };
 
 /* button definitions */
