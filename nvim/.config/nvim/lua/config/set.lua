dd783d14ee87e3b261da1128248dc07f3c8b11a9
AryanAtif <aryanatif01@gmail.com>
2026-07-13 18:14:49 +0500

    Make nvim indentations 2 char long

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -1,10 +1,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.shiftwidth = 4
-vim.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 vim.smartindent = true
 
